a97c868062510d881ee903fdd36b7b9edc32f788
devgasparotto <devgasp@gmail.com>
2017-12-04 18:38:07 -0500

    Individual Registration

--- app/helpers/individual_helper.rb
+++ app/helpers/individual_helper.rb
@@ -1,7 +1,9 @@
 module IndividualHelper
 
 	def RegisterIndividual(firstName, lastName, source, sourceID, email)
+		puts "Attempting to register"
 		if !Individual.exists?(sourceID: sourceID)
+			puts "Individual Does not exist"
 			user = Individual.new(source: source, sourceID: sourceID, firstName: firstName, lastName: lastName, email:email)
 			user.save
 		end
