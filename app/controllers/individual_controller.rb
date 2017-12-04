f7d4282bcaa2632ce2cec5bb31e37dabb9dfcf91
devgasparotto <devgasp@gmail.com>
2017-12-04 18:41:13 -0500

    Individual Registration

--- app/controllers/individual_controller.rb
+++ app/controllers/individual_controller.rb
@@ -1,4 +1,7 @@
 class IndividualController < ApplicationController
+	skip_before_filter :verify_authenticity_token
+	skip_before_action :verify_authenticity_token
+
 	def RegisterFacebookIndividual
 		RegisterIndividual(params['first name'], params['last name'], 'FB', params['messenger user id'], '')
 		render html: "Pass"
