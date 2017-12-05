df3238f595699918bc65cb72283421689ee0b67e
devgasparotto <devgasp@gmail.com>
2017-12-04 19:31:56 -0500

    Helper

--- app/controllers/helper_controller.rb
+++ app/controllers/helper_controller.rb
@@ -2,7 +2,7 @@ class HelperController < ApplicationController
 	skip_before_action :verify_authenticity_token
 
 	def SetIndividualAsHelper
-		ind = Individual.find_by sourceID: params['messenger user id']
+		ind = Individual.find_by_sourceID(params['messenger user id'])
 		if !ind.nil?
 			ind.isHelper = 1
 			ind.isEntreprenuer = 0
