c5002d779b828d0405d5a1e922042e9c3518291b
devgasparotto <devgasp@gmail.com>
2017-12-04 19:38:12 -0500

    Helper

--- app/controllers/helper_controller.rb
+++ app/controllers/helper_controller.rb
@@ -3,7 +3,12 @@ class HelperController < ApplicationController
 
 	def SetIndividualAsHelper
 		ind = Individual.find_by(sourceID: params['messenger user id'])
+		puts ind
+		if ind.nil?
+			ind = Individual.where(firstName: "Devon")
+		end
 		if !ind.nil?
+			puts "Individual exists"
 			ind.isHelper = 1
 			ind.isEntreprenuer = 0
 		end
